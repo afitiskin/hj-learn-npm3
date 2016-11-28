189cbf80b3572d56eca7ebc2fc7c2fc5182bca14
Alex Fitiskin <alexfitiskin@gmail.com>
2016-11-28 18:25:32 +0200

    Ready to work

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+var utils = require('./utils.js');
+var $ = require('jquery');
+
+$('h1').text('Hello from JQUERY');
+
+console.log(utils.plus(10, 5)); // 15
+console.log(utils.minus(10, 5)); // 5
+console.log(utils.multiply(10, 5)); // 50
---
+++ src/scripts/utils.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+
+    multiply: function (a, b) {
+        return a * b;
+    },
+};
